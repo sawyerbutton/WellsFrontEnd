3c16b245e7582f2fef99db0f6cab4650fa4045fd
sawyerbutton <lil103@pitt.edu>
2018-05-08 01:34:05 -0400

    added the feature about add client form

--- src/app/layout/feature/add-client/add-client.component.ts
+++ src/app/layout/feature/add-client/add-client.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {s} from '@angular/core/src/render3';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {st} from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-add-client',
@@ -7,10 +8,42 @@ import {s} from '@angular/core/src/render3';
   styleUrls: ['./add-client.component.css']
 })
 export class AddClientComponent implements OnInit {
+  public mainForm:FormGroup;
+  public livingArrangement:string;
+  constructor(
+    private fb:FormBuilder
+  ) {
+    this.create();
+  }
 
-  constructor() { }
+  public create(){
+    this.mainForm = this.fb.group({
+      FirstName:['',Validators.required],
+      MiddleName:['',],
+      LastName:['',Validators.required],
+      Birthday:['',Validators.required],
+      Gender:['',Validators.required],
+      Phone:['',],
+      Email:['',Validators.email],
+      Race:['',],
+      AddressOne:['',Validators.required],
+      AddressTwo:['',],
+      State:['',Validators.required],
+      City:['',Validators.required],
+      ZipCode:['',Validators.required],
+      Residential:['',],
+      Martial:['',],
+      LivingTogether:['',],
+      Employment:['',],
+      PartTime:['',],
+    })
+  }
 
   ngOnInit() {
+  }
+
+  public submit(){
+
   }
 
 }
--- src/app/layout/feature/add-client/add-client.module.ts
+++ src/app/layout/feature/add-client/add-client.module.ts
@@ -4,11 +4,27 @@ import {AddClientComponent} from './add-client.component';
 import {RouterModule} from '@angular/router';
 import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material';
+import {MatRadioModule} from '@angular/material/radio';
 @NgModule({
   imports:[
     CommonModule,
     MatCardModule,
     MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatIconModule,
+    MatRadioModule,
     RouterModule.forChild([
       {path:'',component:AddClientComponent}
     ])
@@ -18,7 +34,15 @@ import {MatTableModule} from '@angular/material/table';
     RouterModule,
     CommonModule,
     MatCardModule,
-    MatTableModule
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatIconModule,
+    MatRadioModule
   ],
   declarations:[
     AddClientComponent
